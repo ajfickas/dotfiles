722cb4fa39af571d689c02c51eda356caca93cfc
ajfickas <ajfickas@gmail.com>
2020-11-08 16:31:23 -0500

    More gsettings -- animations and mouse acceleration

--- ubuntu/gsettings.sh
+++ ubuntu/gsettings.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
+# Animations
+gsettings set org.gnome.desktop.interface enable-animations false
+
 # Keyboard
 # Set to a different value and then the desired value as a workaround for settings persisting but not being applied after restarting
 # TODO Consider applying these settings automatically upon bluetooth connect event: https://askubuntu.com/questions/259354/how-do-i-connect-to-my-bluetooth-headset
@@ -10,6 +13,8 @@ gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 29
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
 
 # Mouse
+# May help with mouse precision and not honing in on buttons, but not sure
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'adaptive'
 # Set to a different value and then the desired value as a workaround for settings persisting but not being applied after restarting
 # TODO Consider applying these settings automatically upon bluetooth connect event: https://askubuntu.com/questions/259354/how-do-i-connect-to-my-bluetooth-headset
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
